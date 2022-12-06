e2b2bf4f0531df59a828e34b4b7de06b498ee725
CactusCata <adam.chareyre.1999@gmail.com>
2022-12-06 14:22:40 +0100

    lol

--- src/frame/frames/settings/map/fConfigMap.py
+++ src/frame/frames/settings/map/fConfigMap.py
@@ -2,6 +2,10 @@ import frame.rootManager as rootManager
 
 from frame.iFrame import IFrame
 
+from frame.frames.settings.map.fCreateMap import FCreateMap
+from frame.frames.settings.map.fEditMap import FEditMap
+from frame.frames.settings.map.fDeleteMap import FDeleteMap
+
 class FConfigMap(IFrame):
 
     def __init__(self, previousFrame):
@@ -9,3 +13,18 @@ class FConfigMap(IFrame):
 
     def draw(self):
         root = rootManager.getRoot()
+
+        buttonCreateNewMap = super().createButton(text="Créer une map", cmd=lambda: rootManager.runNewFrame(FCreateMap(self)))
+        buttonCreateNewMap.pack()
+
+        buttonEditMap = super().createButton(text="Editer une map", cmd=lambda:rootManager.runNewFrame(FEditMap(self)))
+        buttonEditMap.pack()
+
+        buttonDeleteMap = super().createButton(text="Supprimer une map", cmd=lambda:rootManager.runNewFrame(FDeleteMap(self)))
+        buttonDeleteMap.pack()
+
+        buttonReturn = super().createButton(text="Retour", cmd=lambda:super(FConfigMap, self).reopenLastFrame())
+        buttonReturn.pack()
+
+        buttonHelp = super().createButton(text="Aide")
+        buttonHelp.pack()
--- src/frame/frames/settings/robot/fConfigRobot.py
+++ src/frame/frames/settings/robot/fConfigRobot.py
@@ -2,6 +2,10 @@ import frame.rootManager as rootManager
 
 from frame.iFrame import IFrame
 
+from frame.frames.settings.robot.fCreateRobot import FCreateRobot
+from frame.frames.settings.robot.fEditRobot import FEditRobot
+from frame.frames.settings.robot.fDeleteRobot import FDeleteRobot
+
 class FConfigRobot(IFrame):
 
     def __init__(self, previousFrame):
@@ -9,3 +13,19 @@ class FConfigRobot(IFrame):
 
     def draw(self):
         root = rootManager.getRoot()
+
+
+        buttonCreateRobot = super().createButton(text="Créer robot", cmd=lambda:rootManager.runNewFrame(FCreateRobot(self)))
+        buttonCreateRobot.pack()
+
+        buttonEditRobot = super().createButton(text="Editer robot", cmd=lambda:rootManager.runNewFrame(FEditRobot(self)))
+        buttonEditRobot.pack()
+
+        buttonDeleteRobot = super().createButton(text="Supprimer robot", cmd=lambda:rootManager.runNewFrame(FDeleteRobot(self)))
+        buttonDeleteRobot.pack()
+
+        buttonHelp = super().createButton(text="Aide")
+        buttonHelp.pack()
+
+        buttonBack = super().createButton(text="Retour", cmd=lambda:super(FConfigRobot, self).reopenLastFrame())
+        buttonBack.pack()
